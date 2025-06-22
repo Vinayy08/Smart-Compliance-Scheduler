8170588c750ea3335030cc8d12e0ea1f04a92abb
Vinayy08 <vinaybc1234@gmail.com>
2025-06-22 14:44:59 +0530

    Fix: add get_session to database.py

--- backend/database.py
+++ backend/database.py
@@ -6,9 +6,13 @@
 # def get_session():
 #     with Session(engine) as session:
 #         yield session
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://smartcompliance_db_user:WsbSvLCv0IXsEdU6KElAtTnMTZ5Y95Vx@dpg-d1brkjp5pdvs73e5s7tg-a/smartcompliance_db")
 
 engine = create_engine(DATABASE_URL, echo=True)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
